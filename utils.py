2201e262fa2f117f3548479dd2f1ec0bd0cb3f76
Tevfik Aytekin <tevfik.aytekin@gmail.com>
2020-10-01 14:29:35 +0300

    eda updated

--- utils.py
+++ utils.py
@@ -5,3 +5,12 @@ def sigmoid(x):
 
 def relu(x):
    return np.maximum(0,x)
+
+def calc_contingency_table(df, a1, a2):
+    group1 = np.unique(df[a1])
+    group2 = np.unique(df[a2])
+    con_table = np.zeros((len(group1),len(group2)))
+    for i in range(len(group1)):
+        for j in range(len(group2)):
+            con_table[i][j] = sum((df[a1] == group1[i]) & (df[a2] == group2[j]))
+    return con_table, group1, group2
