86c1feba13a483caf4d87d868eda074bfe9097ab
Tevfik Aytekin <tevfik.aytekin@gmail.com>
2022-09-12 11:53:11 +0300

    update

--- ds_utils.py
+++ ds_utils.py
@@ -48,3 +48,24 @@ def plot_decision_boundary(model, X, y):
         idx = np.where(y == i)
         ax[1].scatter(X[idx, 0], X[idx, 1], c=color, 
                     cmap=plt.cm.RdYlBu, edgecolor='black', s=40)
+        
+def plot_top_words(components, feature_names, n_top_words, title):
+    fig, axes = plt.subplots(2, 5, figsize=(30, 15), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(components):
+        top_features_ind = topic.argsort()[: -n_top_words - 1 : -1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f"Topic {topic_idx +1}", fontdict={"fontsize": 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis="both", which="major", labelsize=20)
+        for i in "top right left".split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
+
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    plt.show()
+
